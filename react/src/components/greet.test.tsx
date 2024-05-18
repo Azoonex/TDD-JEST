bacbb27131547f5494e4d1586920c17d438b2709
amirabas <amirjabic@gmail.com>
2024-05-18 14:49:16 +0330

    testing all and limte test

--- react/src/components/greet.test.tsx
+++ react/src/components/greet.test.tsx
@@ -2,13 +2,13 @@ import {render,screen} from "@testing-library/react"
 import { expect, test } from "vitest"
 import Greet from "./Greet"
 
-test("is Abas in the screen ?",()=>{
+test.only("is Abas in the screen ?",()=>{
     render(<Greet />);
     const textElemtn = screen.getByText(/abas/i);
     expect(textElemtn).toBeInTheDocument();
 })
 
-test("is hello in the componente greet? ",()=>{
+test.skip("is hello in the componente greet? ",()=>{
     render(<Greet />)
     const textElement = screen.getByText('hello')
     expect(textElement).toBeInTheDocument()

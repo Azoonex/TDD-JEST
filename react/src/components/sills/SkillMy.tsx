8b093627f3f8c422e6ce881168c95dab8c764d54
amirabas <amirjabic@gmail.com>
2024-05-25 09:19:30 +0330

    how to rendering test fixed testing for rendering dom

---
+++ react/src/components/sills/SkillMy.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+import { SkillsProps } from "./TypeSkill"
+
+export default function SkillMy({ skill }: SkillsProps) {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+    return (
+        <>
+            <ul>
+                {
+                    skill.map((skill) => {
+                        return <li key={skill}>{skill}</li>
+                    })
+                }
+            </ul>
+            {isLoggedIn ? (
+                <button>Start learning</button>
+            ) : (
+                <button onClick={() => setIsLoggedIn(true)}>Login</button>
+            )}
+        </>
+    )
+}
--- react/src/components/sills/__test__/Skill.test.tsx
+++ react/src/components/sills/__test__/Skill.test.tsx
@@ -1,6 +1,6 @@
 import { screen, render } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
-import SkillMy from "./SkillMy";
+import SkillMy from "../SkillMy";
 
 describe("Skills", () => {
     const skill = ["HTML", "CSS", "JAVASCRIPT"];
@@ -11,9 +11,22 @@ describe("Skills", () => {
         expect(listElemnt).toBeInTheDocument();
     });
 
-    test("renders a list of skills",()=>{
+    test("renders a list of skills", () => {
         render(<SkillMy skill={skill} />)
         const listitemElement = screen.getAllByRole('listitem')
         expect(listitemElement).toHaveLength(3)
+    })
+
+    test("renders Login button", () => {
+        render(<SkillMy skill={skill} />);
+        const loginButton = screen.getByRole("button");
+        expect(loginButton).toBeInTheDocument()
+    })
+
+    test("render login for Dom", () => {
+        const buttonLoing = screen.queryByRole("button",{
+            name: "Login"
+        })
+        expect(buttonLoing).not.toBeInTheDocument()
     })
 });

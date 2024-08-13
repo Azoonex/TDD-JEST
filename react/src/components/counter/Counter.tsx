1b4f378a5919e826055ab640b0ef4aa6aa89f5a6
amirabas <amirjabic@gmail.com>
2024-08-13 19:20:36 +0330

    get start course jest 17

--- react/src/components/counter/Counter.tsx
+++ react/src/components/counter/Counter.tsx
@@ -2,10 +2,17 @@ import  { useState } from 'react'
 
 export default function Counter() {
     const [count, setCount] = useState(0);
+    const [amount,setAmount] = useState(0);
     return (
         <div>
-            <h3>{count}</h3>
+            <h1>{count}</h1>
             <button onClick={() => setCount(count + 1)}>Increment</button>
+            <input type="text"
+                name='amout'
+                value={amount}
+                onChange={(e)=> setAmount(parseInt(e.target.value))}
+            />
+            <button onClick={() => setCount(0)}>Set</button>
         </div>
     )
 }

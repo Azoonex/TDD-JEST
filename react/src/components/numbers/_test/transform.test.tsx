f050a8a2233a999ea663a8c41333de3106bb68c9
amirabas <amirjabic@gmail.com>
2024-08-25 13:13:18 +0330

    create new test for check ageMan

--- react/src/components/numbers/_test/transform.test.tsx
+++ react/src/components/numbers/_test/transform.test.tsx
@@ -1,19 +1,27 @@
 import { it, expect } from "vitest";
-import { transfromNum } from "../transfrom";
+import { AcceptableAge, transfromNum } from "../transfrom";
 
 it('check the value it is a number or NaN', () => {
     let value = 22
-    
+
     let result = transfromNum(value)
-    
+
     expect(result).toBeTypeOf('number')
 })
 
 
-it('check the thit is Nan',()=>{
+it('check the thit is Nan', () => {
+    //    Arrange
     let value = 'amirabas';
-
+    // Act
     let result = transfromNum(value);
-
+    // Assert
     expect(result).toBeNaN()
+
+    // checkAcceptabel Age
+    let ageMan = 44;
+
+    let resultAge = AcceptableAge(22)
+
+    expect(resultAge).toBeTypeOf('boolean')
 })
--- react/src/components/numbers/transfrom.ts
+++ react/src/components/numbers/transfrom.ts
@@ -1,3 +1,8 @@
-export function transfromNum(i: number){
-    return ++i
+export function transfromNum(i: number) {
+    return ++i;
+}
+
+export function AcceptableAge(age: number) {
+    if (age >= 24) return true;
+    return false;
 }
